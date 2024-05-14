70463f8c0a4441be8c8206a740babcdaedbfcbba
hyotaime <hyotaime@hyotaime.com>
2024-05-15 01:09:56 +0900

    refactor: Split main.py to hand_pipe.py, pinch.py, common.py by using OOP

---
+++ src/common.py
@@ -0,0 +1,33 @@
+def get_hand_avg_pos(hand_landmarks, frame_shape):
+    THUMB_TIP = 4
+    INDEX_TIP = 8
+    MIDDLE_TIP = 12
+    RING_TIP = 16
+    PINKY_TIP = 20
+
+    thumb_tip = hand_landmarks.landmark[THUMB_TIP]
+    index_tip = hand_landmarks.landmark[INDEX_TIP]
+    middle_tip = hand_landmarks.landmark[MIDDLE_TIP]
+    ring_tip = hand_landmarks.landmark[RING_TIP]
+    pinky_tip = hand_landmarks.landmark[PINKY_TIP]
+
+    # 평균 좌표 계산
+    avg_x = (thumb_tip.x + index_tip.x + middle_tip.x + ring_tip.x + pinky_tip.x) / 5
+    avg_y = (thumb_tip.y + index_tip.y + middle_tip.y + ring_tip.y + pinky_tip.y) / 5
+
+    # 이미지 좌표로 변환
+    return int(avg_x * frame_shape[1]), int(avg_y * frame_shape[0])
+
+def calculate_distance(hand1, hand2):
+    """
+    두 손의 검지 랜드마크를 사용하여 거리 계산
+    """
+    INDEX_TIP = 8
+    hand1_index_tip = hand1.landmark[INDEX_TIP]
+    hand2_index_tip = hand2.landmark[INDEX_TIP]
+
+    # 거리 계산
+    dx = hand1_index_tip.x - hand2_index_tip.x
+    dy = hand1_index_tip.y - hand2_index_tip.y
+
+    return (dx ** 2 + dy ** 2) ** 0.5
---
+++ src/pinch.py
@@ -0,0 +1,18 @@
+def is_pinch(hand_landmarks, image):
+    # 엄지와 검지의 랜드마크를 이용하여 두 손가락이 닿았는지 확인
+    THUMB_TIP = 4
+    INDEX_TIP = 8
+
+    # 랜드마크 좌표 가져오기
+    thumb_tip = hand_landmarks.landmark[THUMB_TIP]
+    index_tip = hand_landmarks.landmark[INDEX_TIP]
+
+    # 좌표를 이미지 좌표로 변환
+    thumb_pos = (int(thumb_tip.x * image.shape[1]), int(thumb_tip.y * image.shape[0]))
+    index_pos = (int(index_tip.x * image.shape[1]), int(index_tip.y * image.shape[0]))
+
+    # 두 좌표 간의 거리 계산
+    distance = ((thumb_pos[0] - index_pos[0]) ** 2 + (thumb_pos[1] - index_pos[1]) ** 2) ** 0.5
+
+    # 일정 거리 내에 있으면 접촉으로 간주
+    return distance < 50
